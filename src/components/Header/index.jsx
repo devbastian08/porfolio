6e50cf3d3f49a893de4860a5230dd71579fa7b9d
devbastian08 <sebastian08.dev@gmail.com>
2023-08-15 23:36:53 -0500

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import "./Header.css"
+import PhotoHeader from "../PhotoHeader";
+
+
+const Header = ()=>{ 
+
+    return (
+        <div className="header">
+            <nav className="navbar container">
+            <a href="#" className="logo"><img src="./img/logo.png" alt="" /></a>
+            <input type="checkbox" id="toggler" />
+            <label htmlFor="toggler"><i className="ri-menu-line"></i></label>
+            <div className="menu">
+                <ul className="list">
+                    <li><a href="#">Sobre Mi</a></li>
+                    <li><a href="#">Habilidades</a></li>
+                    <li><a href="#">Proyectos</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </div>
+        </nav>
+        <PhotoHeader/>
+        </div>
+    )
+
+}
+
+export default Header;
---
+++ src/components/PhotoHeader/index.jsx
@@ -0,0 +1,15 @@
+import "./PhotoHeader.css";
+
+const PhotoHeader = ()=>{
+
+    return (
+        <div className="photoheader">
+            <img src="./img/bastian.png" alt="sebastian" />
+            <div className="texto">
+            <p className="p1">Hola, soy Sebastian trujillo</p>
+            <p className="p2">Desarrollador FrontEnd</p>
+            </div>
+        </div>
+    )
+}
+export default PhotoHeader; 
